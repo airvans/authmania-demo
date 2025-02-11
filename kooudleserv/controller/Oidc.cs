a26c57fac3fae024c7ca6bc782741e70c8b3d535
airvans <evanarugu@gmail.com>
2025-02-12 00:07:33 +0100

    starting with them views old lols and cry at man city loss

--- kooudleserv/controller/Oidc.cs
+++ kooudleserv/controller/Oidc.cs
@@ -15,13 +15,29 @@ public class Oidccontoller:Controller{
     [Route(".well-known/openid-configuration")]
     [HttpGet]
     public IActionResult index(){
-        return Ok();
+        return Ok(new{          
+            issuer = "http://localhost:5264",
+            authorization_endpoint = "http://localhost:5264/oauth/authorize",
+            token_endpoint = "http://localhost:5264/oauth/token",
+            userinfo_endpoint = "http://localhost:5264/userinfo",
+            id_token_signing_alg_values_supported = new[] {"HS256"}
+        });
     }
 
     [Route("userinfo")]
     [HttpGet]
     public IActionResult get(){
-        return Ok();
+        return Ok(new
+    {
+        sub = "demo-user123", 
+        email = "test@gmail.com",
+        email_verified = true,
+        name = "demo-user",
+        given_name = "Demo",
+        family_name = "User",
+        preferred_username = "demo-user",
+        kooudle = "yes-it-works"
+    });
     }
 
    [HttpGet("authorize")]
